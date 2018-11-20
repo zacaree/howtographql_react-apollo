e6fdd5ea30c49dabffbfc0a40eeaa06c54c529f5
Zac <zacaree@gmail.com>
2018-11-20 16:16:53 -0600

    checkpoint: EOD

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+// import gql from 'graphql-tag';
+// import { client } from '../index';
 
 export default class Link extends Component {
   render() {
     const { description, url } = this.props.link;
     return (
       <div>
-        {description} ({url})
+        <p>{description} ({url})</p>
+
       </div>
     )
   }
 }
+
--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 // import { ApolloProvider } from 'react-apollo';
-// import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+
 
 
 // import { resolvers, defaults } from './resolvers';
 
 
-// const client = new ApolloClient({
-//   uri: 'http://localhost:4000',
-//   clientState: {
-//     defaults,
-//     resolvers,
-//   }
-// });
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  clientState: {
+    defaults: {},
+    resolvers: {}
+  }
+});
 
 
 
@@ -32,3 +34,18 @@ const Root = () => {
 
 
 ReactDOM.render(<Root />, document.getElementById('root'));
+
+
+
+client.query({
+  query: gql`
+    {
+      feed {
+        links {
+          id
+          description
+        }
+      }
+    }
+  `
+}).then(response => console.log(response.data.feed.links))
