9cbcda5a2dd77206b4fc9ac237102f94e9c8f969
zacaree <zacharem@gmail.com>
2018-11-21 15:36:26 -0600

    Checkpoint: Finished the Authentication lesson.

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './components/App';
 import { ApolloProvider } from 'react-apollo';
-import ApolloClient from 'apollo-boost';
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
 import { AUTH_TOKEN } from './constants';
 // import gql from 'graphql-tag';
